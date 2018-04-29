dcdc2a9d9a8e802878c3b57261fbcfcf59f9cac9
Pimy <Vespimian@gmail.com>
2018-04-29 23:58:48 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 //VARS
-var loopInterval = 100; //loop speed
+var loopInterval = 50; //loop speed
 var cLeft = 0; //character left
 var cTop = 0; //character top
 var cMove = 1; //number of pixels the character moves per move
 var cStartL = cMove * 3;
 var cStartT = cMove * 3;
+var punchyBlock = 2;
 //Player Variables
+var lastDirection;
 var level = 0;
 var xp = 0;
 
@@ -14,6 +16,8 @@ var xp = 0;
 function setup() {//first initialization setup
     drawMap();
     loop();
+
+    console.log("Setup Completed");
 } window.onload = setup();
 
 
@@ -21,7 +25,7 @@ function loop() {//game loop
     setInterval(function(){
 
     	detectBlock();
-
+    	collisionDetect();
 
     	clearMap();
     	drawMap();
@@ -46,26 +50,37 @@ function clearMap() {//clear the map
 }
 
 function move(e) { //character movement
-    if (e.keyCode == 39) { 
+    if (e.keyCode == 39) { //right arrow
         cLeft += cMove; 
         mapArray[cTop][cLeft - 1] = mapOriginal[cTop][cLeft - 1];
         mapArray[cTop][cLeft] = 0;
+        lastDirection = "right";
     } 
-    else if (e.keyCode == 37) {
+    else if (e.keyCode == 37) { //left arrow
         cLeft -= cMove; 
         mapArray[cTop][cLeft + 1] = mapOriginal[cTop][cLeft + 1];
         mapArray[cTop][cLeft] = 0;
+        lastDirection = "left";
     }
-    else if (e.keyCode == 40) {
+    else if (e.keyCode == 40) { //down arrow
         cTop += cMove; 
         mapArray[cTop - 1][cLeft] = mapOriginal[cTop - 1][cLeft];
         mapArray[cTop][cLeft] = 0;
+        lastDirection = "down";
     }
-    else if (e.keyCode == 38) {
+    else if (e.keyCode == 38) { //up arrow
         cTop -= cMove; 
         mapArray[cTop + 1][cLeft] = mapOriginal[cTop + 1][cLeft];
         mapArray[cTop][cLeft] = 0;
+        lastDirection = "up";
     }
+
+
+    if(e.keyCode == 32){
+    	if(mapArray[cTop][cLeft + 1] == punchyBlock || mapArray[cTop][cLeft - 1] == punchyBlock || mapArray[cTop + 1][cLeft] == punchyBlock || mapArray[cTop - 1][cLeft] == punchyBlock){
+		mapArray[cTop][cLeft + 1] = 1;
+		}
+	}
 } document.onkeydown = move;
 
 function detectBlock() {//detect when the user is standing at certain coordinates
@@ -77,4 +92,27 @@ function detectBlock() {//detect when the user is standing at certain coordinate
 		mapOriginal[5][6] = 10;
 	}
 	setTimeout(function(){}, loopInterval)
+}
+
+function collisionDetect() {
+	if (lastDirection == "down" && mapOriginal[cTop][cLeft] == 5){
+		mapArray[cTop --][cLeft] = 5;
+		mapArray[cTop][cLeft] = 0;
+		//console.log("water down");
+	}
+	else if (lastDirection == "up" && mapOriginal[cTop][cLeft] == 5){
+		mapArray[cTop ++][cLeft] = 5;
+		mapArray[cTop][cLeft] = 0;
+		//console.log("water up");
+	}
+	else if (lastDirection == "left" && mapOriginal[cTop][cLeft] == 5){
+		mapArray[cTop][cLeft ++] = 5;
+		mapArray[cTop][cLeft] = 0;
+		//console.log("water left");
+	}
+	else if (lastDirection == "right" && mapOriginal[cTop][cLeft] == 5){
+		mapArray[cTop][cLeft --] = 5;
+		mapArray[cTop][cLeft] = 0;
+		//console.log("water right");
+	}
 }
